65882262b6b960adc98889a463af2a4512080588
yukihirop <te108186@gmail.com>
2016-06-19 10:53:53 +0900

    Chapter10完了
    - ルーティングについて

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -2,9 +2,15 @@ class Admin::TopController < Admin::Base
 
   def index
 
+    if current_administrator
+      render action: 'dashboard'
+    else
+      render action: 'index'
+    end
+
     # IpAddressRejectedのエラーをわざと発生
     # raise IpAddressRejected
-    render action: 'index'
+    # render action: 'index'
   end
 
 end
